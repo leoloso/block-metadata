1e8a8fe989b2101adeb6f83acb06ab75e9973ab3
Leo <losoviz@gmail.com>
2019-08-23 23:22:39 +0800

    Enabled WPGraphQL code

--- src/load.php
+++ src/load.php
@@ -5,4 +5,4 @@ require_once( dirname( __FILE__ ) . '/Metadata.php' );
 require_once( dirname( __FILE__ ) . '/RESTUtils.php' );
 require_once( dirname( __FILE__ ) . '/RESTEndpoints.php' );
 require_once( dirname( __FILE__ ) . '/RESTHooks.php' );
-// require_once( dirname( __FILE__ ) . '/WPGraphQL.php' );
+require_once( dirname( __FILE__ ) . '/WPGraphQL.php' );
