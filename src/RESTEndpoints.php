e3e9aba63f6543e9c44facbcca91bd84339d3769
Declan Rek <declanrek@gmail.com>
2020-03-04 17:03:49 +0100

    add endpoint to get all posts in block format

--- src/RESTEndpoints.php
+++ src/RESTEndpoints.php
@@ -40,4 +40,27 @@ class RESTEndpoints {
         $response->set_status(200);
         return $response;
     }
+
+    public static function get_all_post_block_meta($request) 
+    {
+        $args = array(
+            'numberposts' => -1
+        );
+        $posts = \get_posts($args);
+
+        $result = array();
+        foreach($posts as $post) {
+            $item_metadata = array();
+            $item_metadata['id'] = $post->ID;
+            $item_metadata['title'] = $post->post_title;
+            
+            $block_data = Data::get_block_data($post->post_content);
+            $block_metadata = Metadata::get_block_metadata($block_data);
+            $item_metadata['blocks'] = $block_metadata;
+            $result[] = $item_metadata;
+        }
+        $response = new \WP_REST_Response($result);
+        $response->set_status(200);
+        return $response;
+    }
 }
--- src/RESTHooks.php
+++ src/RESTHooks.php
@@ -27,5 +27,12 @@ class RESTHooks {
                 'callback' => [RESTEndpoints::class, 'get_post_block_meta']
             ]);
         });
+        \add_action('rest_api_init', function () {
+            // Endpoint: /wp-json/block-metadata/v1/metadata/
+            \register_rest_route(RESTUtils::get_namespace(), 'metadata', [
+                'methods'    => 'GET',
+                'callback' => [RESTEndpoints::class, 'get_all_post_block_meta']
+            ]);
+        });
     }
 }
